177d495cebe81d19b8a0e20784465b5eac283442
Megan Talbert <megantalbert@Megans-MacBook-Pro.local>
2021-11-16 10:39:14 -0800

    added home page url

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.http import render
 from .models import Question
 from django.template import loader
 
