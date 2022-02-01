ec8477de7d70d29a4d78ea34159d92fe5bb25f85
Megan Talbert <megantalbert@Megans-MacBook-Pro.local>
2022-02-01 09:29:38 -0800

    better naming for folders

--- database_help/dal.py
+++ database_help/dal.py
@@ -8,7 +8,8 @@ import datetime
 #DB_URL="postgresql://localhost" DB_NAME="thisdb" RAW_DATA_PATH="../testdata" python data_loader.py
 #**ADD IN URL user and password
 thisuser = 'test2'
-desired_time_start = datetime.datetime(year=1970, month=1, day=1)
+desired_time_start = datetime.datetime(year=1970, month=1, day=1, tzinfo=datetime.timezone.utc).timestamp()
+desired_time_end = datetime.datetime(year=1970, month=1, day=1, second=4, tzinfo=datetime.timezone.utc).timestamp()
 
 
 db_url = os.environ['DB_URL']
@@ -17,7 +18,7 @@ data_path = os.environ['RAW_DATA_PATH']
 
 engine = sqlalchemy.create_engine(f'{db_url}/{db_name}', echo=False)
 query = f'''
-SELECT * FROM measurements WHERE userid='{thisuser}' ORDER BY time DESC
+SELECT * FROM measurements WHERE userid='{thisuser}' AND time BETWEEN {desired_time_start} AND {desired_time_end} ORDER BY time DESC
 
 
 '''
